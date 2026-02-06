db5ee8847f143b783bf90a4cda85ba1bf274476d
Chaojie1 <chaojiew2@nycstudents.net>
2026-02-06 11:02:18 -0800

    idk what to do 😭

--- app/src/views/homepage.vue
+++ app/src/views/homepage.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
-
+        <button @click="relocate"></button>
     </div>
 </template>
 
 <script setup>
-
+    import { ref } from 'vue'
+    function relocate(){
+        document.querySelector('button').classList = `absolute top-${Math.round(Math.random)} left-0`
+    }
 </script>
 
 <style scoped>
