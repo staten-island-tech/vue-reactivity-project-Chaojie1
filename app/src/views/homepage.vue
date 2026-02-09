bc49123957b8c205b587d925be077b125e086cc4
Chaojie1 <chaojiew2@nycstudents.net>
2026-02-09 11:03:08 -0800

    aimingmytrain

--- app/src/views/homepage.vue
+++ app/src/views/homepage.vue
@@ -1,13 +1,19 @@
 <template>
     <div>
-        <button @click="relocate"></button>
+        <button @click="relocate">hi</button>
     </div>
 </template>
 
 <script setup>
     import { ref } from 'vue'
+    function clamp(min, max, value) {
+    return Math.min(Math.max(value, min), max);
+    }
     function relocate(){
-        document.querySelector('button').classList = `absolute top-${Math.round(Math.random)} left-0`
+        const button = document.body.querySelector('button');
+        const max = [window.innerWidth  - button.offsetWidth,window.innerHeight - button.offsetHeight];
+        const newpos = [Math.round(Math.random() * 250)*((-1)**Math.ceil(Math.random() * 4)),Math.round(Math.random() * 250)*((-1)**Math.ceil(Math.random() * 4))]
+        button.style.transform = `translate(${clamp(0,max[0],newpos[0]+max[0]/2)}px, ${clamp(0,max[1],newpos[1]+max[1]/2)}px)`;
     }
 </script>
 
